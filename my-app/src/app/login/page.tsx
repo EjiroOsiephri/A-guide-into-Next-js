06250ecd5f3f5a5f580a349f143fd3799a2bd64c
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-09-04 02:03:18 +0100

    Working with mongo db

---
+++ my-app/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <>
+      <h1>Found in bikini bottom</h1>
+    </>
+  );
+};
+
+export default Login;
---
+++ my-app/src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+const Signup = () => {
+  return (
+    <>
+      <h1>Lost in bikini bottom</h1>
+    </>
+  );
+};
+
+export default Signup;
