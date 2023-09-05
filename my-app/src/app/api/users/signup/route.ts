b944575a396e4c3ef5d366b78b9c9859aea6d3a3
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-09-05 21:30:02 +0100

    finished api creation

--- my-app/src/app/api/users/signup/route.ts
+++ my-app/src/app/api/users/signup/route.ts
@@ -6,6 +6,33 @@ connect();
 
 export async function Post(request: NextRequest) {
   try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exist" },
+        { status: 500 }
+      );
+    }
+    const bcrypt = require("bcryptjs");
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+      email,
+    });
+
+    const savedUser = await newUser.save();
+    console.log(savedUser);
+
+    return NextResponse.json(
+      { message: "User created succesfully", savedUser },
+      { status: 201 }
+    );
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
